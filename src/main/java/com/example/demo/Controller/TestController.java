3c4083bd429d3bc1af80f52ae34578ae01561792
wuchao <964498622@qq.com>
2019-10-23 16:32:48 +0800

    第一次上传整体代码

---
+++ src/main/java/com/example/demo/Controller/TestController.java
@@ -0,0 +1,13 @@
+package com.example.demo.Controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    @GetMapping("/test")
+    public String findDemoById(){
+        return  "success";
+    }
+}
